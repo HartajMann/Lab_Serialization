432d2a9493ac3f8e1517f2bd62caced1165bfe5d
Hartaj Singh Mann <99726645+HartajMann@users.noreply.github.com>
2023-03-27 19:22:52 -0600

    Add project files.

---
+++ Lab_Serialization/Event.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Serialization
+{
+	[Serializable] 
+	public class Event
+	{
+		public int eventNumber { get; set; }
+		public string location { get; set; }
+
+		public void ReadFromFile(string Path)
+		{
+			using (StreamWriter sw = new StreamWriter(Path))
+			{
+				sw.Write("Hackathon");
+			}
+
+			using (StreamReader sr = new StreamReader(Path))
+			{
+				string word = sr.ReadToEnd();
+				Console.WriteLine("In Word: " + word);
+				Console.WriteLine("The First Character is: " +word[0]);
+				Console.WriteLine("The Middle Character is: " + word[word.Length / 2]);
+				Console.WriteLine("The Last Character is: " + word[word.Length - 1]);
+			}
+		}
+	}
+}
---
+++ Lab_Serialization/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Linq;
+
+namespace Lab_Serialization
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Event e1 = new Event();
+			e1.eventNumber = 1;
+			e1.location = "Calgary";
+			string path = @"C:\Users\Harthaj\Documents\sait-sem2\oops_2\Lab_Serialization\Lab_Serialization\event.txt";
+			BinaryFormatter bf = new BinaryFormatter();
+			using (FileStream fs = new FileStream(path, FileMode.Open))
+			{
+				bf.Serialize(fs, e1);
+			}
+			using (FileStream fs = new FileStream(path, FileMode.Open))
+			{
+				Event newEvent = (Event)bf.Deserialize(fs);
+				Console.WriteLine(newEvent.eventNumber);
+				Console.WriteLine(newEvent.location);
+			}
+			e1.ReadFromFile(path);
+			Console.ReadKey();
+		}
+	}
+}
